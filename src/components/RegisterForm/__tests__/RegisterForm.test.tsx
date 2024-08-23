d908f12bd86fb354084e5289c0e90f98c85d5aa5
Paul Glenn <paglenn@users.noreply.github.com>
2024-08-23 13:54:06 -0400

    removed breaking module

--- src/components/RegisterForm/__tests__/RegisterForm.test.tsx
+++ src/components/RegisterForm/__tests__/RegisterForm.test.tsx
@@ -3,9 +3,10 @@
  */
 import * as React from "react";
 import { render, screen } from "@testing-library/react";
-
 import { test, expect, describe } from "@jest/globals";
 import RegisterForm from "../RegisterForm";
+
+import userEvent from "@testing-library/user-event";
 
 describe("Register Form", () => {
   test("register form renders properly", () => {
@@ -21,5 +22,26 @@ describe("Register Form", () => {
 
   test("Register form submit button calls event handler", () => {
     render(<RegisterForm />);
+    const submitButton = screen.getByText("Submit");
+
+    // act
+    userEvent.click(submitButton);
+
+    // assert
+  });
+  test("Reset button resets fields", () => {
+    render(<RegisterForm />);
+    const resetButton = screen.getByText("Reset Fields");
+    const firstNameField = screen.getAllByRole("textbox")[0];
+    console.log(firstNameField.textContent);
+    // act
+    userEvent.click(firstNameField);
+    userEvent.paste("Paul");
+    console.log(firstNameField.textContent);
+    //check that it has been pasted
+    //expect(firstNameField);
+    userEvent.click(resetButton);
+
+    // assert
   });
 });
